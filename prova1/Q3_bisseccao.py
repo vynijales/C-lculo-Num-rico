a80d07b2bc651c0c5327b650b2501c64071ff0b4
Nickolas Emanuel <nickolasemanuel10@gmail.com>
2024-12-06 08:59:41 -0300

    corrigindo a q3

--- prova1/Q3_bisseccao.py
+++ prova1/Q3_bisseccao.py
@@ -15,6 +15,7 @@ import math
 def f(x):
     return x * x + x - 6.0
 
+# Precisão
 eps = 0.001
 
 def bis(a, b, k=None, iterations=0):
@@ -24,26 +25,40 @@ def bis(a, b, k=None, iterations=0):
     
     c = (a + b) / 2  # Ponto médio do intervalo
     
+    # Verificações de parada (raízes nos extremos)
     if abs(f(a)) < eps:
         return a, iterations + 1
     elif abs(f(b)) < eps:
         return b, iterations + 1
 
+    # Verifica se o número de iterações foi excedido
     if k < 0:
         return None, iterations + 1
 
     if abs(f(c)) < eps:
         return c, iterations + 1
 
-    # Recursão nos subintervalos
+    # Recursão nos subintervalos (Caso haja raízes nos subintervalos)
     if f(a) * f(c) < 0:
         return bis(a, c, k - 1, iterations + 1)
     elif f(c) * f(b) < 0:
         return bis(c, b, k - 1, iterations + 1)
+    
+    # Recursão nos subintervalos (Caso não haja raízes nos subintervalos)
+    r1 = bis(a, c, k-1)
+    r2 = bis(c, b, k-1)
+    
+    if r1 is None:
+        return r2
+    elif r2 is None:
+        return r1
+    # Caso haja mais de uma raiz nos subintervalos, retorna a raiz mais próxima de 0
+    else:
+        return min(abs(f(r1)), abs(f(r2)))
 
-if __name__ == "__main__":
-    # Testando a função
-    raiz, total_iteracoes = bis(0, 3)
 
-    # Imprimindo a raiz e o total de iterações
-    print(f"Raiz encontrada pelo método da bissecção: {raiz} em {total_iteracoes} iterações")
+# Testando a função
+raiz, total_iteracoes = bis(0, 3)
+
+# Imprimindo a raiz e o total de iterações
+print(f"Raiz encontrada pelo método da bissecção: {raiz} em {total_iteracoes} iterações")
